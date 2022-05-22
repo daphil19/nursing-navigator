1de6143f570d338ba7a0f8da5c48b06169f5ee6d
Davd Phillips <daphillips19@gmail.com>
2022-05-21 21:26:47 -0400

    move back to latest AGP since gateway doesn't work for android emulation yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 val composeVersion by extra("1.1.1")
 
 plugins {
-    id("com.android.application") version "7.0.4" apply false
-    id("com.android.library") version "7.0.4" apply false
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     kotlin("plugin.serialization") version "1.6.10" apply false
 }
